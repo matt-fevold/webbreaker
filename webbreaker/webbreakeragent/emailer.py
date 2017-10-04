fdb0124d815c46fe2c85c2552998706d17770438
Matthew Dunaj <mdunaj@umich.edu>
2017-10-04 09:58:00 -0500

    Add agent emailer settings read

--- webbreaker/webbreakeragent/emailer.py
+++ webbreaker/webbreakeragent/emailer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
+from subprocess import CalledProcessError
 try:
     from email.MIMEMultipart import MIMEMultipart
     from email.MIMEText import MIMEText
@@ -11,11 +12,19 @@ except ImportError: #Python3
 import smtplib
 from webbreaker.webbreakerlogger import Logger
 
+try:
+    import ConfigParser as configparser
+except ImportError: #Python3
+    import configparser
+
+try:  # Python 2
+    config = configparser.SafeConfigParser()
+except NameError:  # Python 3
+    config = configparser.ConfigParser()
 
 class EmailNotifier(object):
     def __init__(self):
-        # TODO: Read and init from email.ini
-        pass
+        self.__read_settings__()
 
     def notify(self, recipient, subject, git_url, ssc_url):
         try:
@@ -34,3 +43,17 @@ class EmailNotifier(object):
         except (Exception, AttributeError) as e:  # we don't want email failure to stop us, just log that it happened
             Logger.app.error("Error sending email. {}".format(e.message))
             Logger.console.error("Error sending email, see log: {}!".format(Logger.app_logfile))
+
+    def __read_settings__(self):
+        settings_file = os.path.abspath(os.path.join('webbreaker', 'webbreakeragent', 'email.ini'))
+        try:
+            config.read(settings_file)
+            self.smtp_host = config.get("emailer", "smtp_host")
+            self.smtp_port = config.get("emailer", "smtp_port")
+            self.from_address = config.get("emailer", "from_address")
+            self.email_template = config.get("emailer", "email_template")
+
+        except (configparser.NoOptionError, CalledProcessError) as noe:
+            Logger.console.error("{} has incorrect or missing values {}".format(settings_file, noe))
+        except configparser.Error as e:
+            Logger.app.error("Error reading {} {}".format(settings_file, e))
