edc076adf96a756b53172961b841a3543585f0ad
BrandonMSpruth <brandon.spruth2@target.com>
2017-10-11 10:27:43 -0500

    removed package_data

--- setup.py
+++ setup.py
@@ -79,7 +79,6 @@ try:
         packages=find_packages(exclude=['docs', 'images', 'tests*']),
         include_package_data=True,
         zip_safe=True,
-        #package_data={'etc': ['*.ini']},
         install_requires=requires,
         entry_points={
                     'console_scripts':[
