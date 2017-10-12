7a805fc58fce4daaff61aa675db5fddc2fc9c3de
xolian <bspruth@gmail.com>
2017-10-11 22:26:36 -0500

    added travis deploy section

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ set -e
 git clean -fXd
 
 python setup.py bdist_wheel --universal
-python setup.py bdist_egg
-python setup.py sdist --formats=bztar,zip,gztar,tar
+python setup.py build
+python setup.py publish
