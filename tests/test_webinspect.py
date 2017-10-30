e6590d528de13c5be7e8b2eef49c93cac6e28953
KylerWitting <kyler.witting@target.com>
2017-10-30 08:34:19 -0500

    Clean up comments for TODO

--- tests/test_webinspect.py
+++ tests/test_webinspect.py
@@ -21,83 +21,6 @@ def general_exception():
     raise Exception('Test Failure')
 
 
-# def test_webinspect_scan_req(runner):
-#     result = runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'Default'])
-#     print(result.output)
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_scan_name(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--scan_name'])
-#     assert result.exit_code == 0
-#
-#
-# # def test_webinspect_scan_size(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--size'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_scan_mode(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--scan_mode'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_scan_scope(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--scan_scope'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_login_macro(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--login_macro'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_scan_policy(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--scan_policy'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_scan_start(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--scan_start'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_start_urls(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--start_urls'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_upload_settings(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--upload_settings'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_upload_policy(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--upload_policy'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_upload_webmacros(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--upload_webmacros'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_fortify_user(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--fortify_user'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_allowed_hosts(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--allowed_hosts'])
-#     assert result.exit_code == 0
-#
-#
-# def test_webinspect_scan_workflow_macros(runner):
-#     result = runner.invoke(webbreaker,  ['scan', '--workflow_macros'])
-#     assert result.exit_code == 0
-#
-
-
 @mock.patch('webbreaker.__main__.WebinspectQueryClient')
 def test_webinspect_download_req_no_scans_found(test_mock, runner, caplog):
     test_mock.return_value.get_scan_by_name.return_value = []
@@ -271,3 +194,5 @@ def test_webinspect_list_protocol_change_success(test_mock, runner, caplog):
     assert result.exit_code == 0
 
 # TODO: webbreaker webinespect download
+
+# TODO: webinspect scan [OPTIONS]
