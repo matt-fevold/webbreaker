7aac710488fdc4799ba88a418f766f8407469817
Brandon Spruth <bspruth@gmail.com>
2017-12-07 21:12:09 -0600

    refactored filename
    removed comments and refactored filename

--- build.ps1
+++ build.ps1
@@ -1,8 +1,5 @@
 #!/bin/sh
-# pyinstaller --clean -y --windowed webbreaker.spec
-# --paths /Library/Python/2.7/site-packages
-# hdiutil create ./webbreaker.dmg -srcfolder webbreaker.app -ov
-# pyinstaller --clean -y --onefile --dist $(pwd)/dist --name webbreaker-cli $(pwd)/webbreaker/__main__.py 
+
 
 try 
 {
@@ -29,8 +26,6 @@ try
         $python = get-command python.exe
         if (test-path setup.py) 
         {
-                #pip install pyOpenSSL
-                #python setup.py build
                 pip install -r requirements.txt
                 python setup.py install
                 pyinstaller.exe --clean -y --onefile --distpath $pwd\dist --name webbreaker $pwd\webbreaker\__main__.py 
