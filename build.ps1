5b87a027101037e3ba9e1739c4c7e2825273cd04
Aaron Tesch <aaron.tesch@target.com>
2018-03-05 16:07:40 -0600

    updated binary name to have version and OS (#128)

--- build.ps1
+++ build.ps1
@@ -23,12 +23,19 @@ catch
 
 try
 {
+        switch -regex ((get-wmiobject win32_operatingsystem).Name) {
+                "Windows 7"  {$operatingSystem = "windows7" }
+                "Windows 8"  {$operatingSystem = "windows8" }
+                "Windows 10" {$operatingSystem = "windows10" }
+        }
+        $version = (gc .\webbreaker\__init__.py | ConvertFrom-StringData).__version__.replace("'","")
+        $binaryName = "webbreaker-"+$version+"-"+$operatingSystem
         $python = get-command python.exe
         if (test-path setup.py) 
         {
                 pip install -r requirements.txt
                 python setup.py install
-                pyinstaller.exe --clean -y --onefile --distpath $pwd\dist --name webbreaker $pwd\webbreaker\__main__.py 
+                pyinstaller.exe --clean -y --onefile --distpath $pwd\dist --name $binaryName $pwd\webbreaker\__main__.py 
         }
         else 
         {
