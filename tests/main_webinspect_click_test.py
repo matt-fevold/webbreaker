65c347f37040e8c75d8701ae7a8064442f80180c
Matt-fevold <matt.fevold@gmail.com>
2018-03-28 14:02:45 -0500

    add click tests for fortify - fix formatting in webinspect click tests

--- tests/main_webinspect_click_test.py
+++ tests/main_webinspect_click_test.py
@@ -58,103 +58,106 @@ class WebInspectResponseTest(object):
         else:
             return json.dumps(self.data)
 
+
 @mock.patch('webbreaker.__main__.WebInspectListServers.__init__')
 def test_webinspect_list_servers(list_server_mock, runner):
     list_server_mock.return_value = None
 
-    # Run the CLI proxy --list
+    # Run the CLI --servers
     runner.invoke(webbreaker, ['webinspect', '--servers'])
 
     list_server_mock.assert_called_once_with()
+
 
 @mock.patch('webbreaker.__main__.WebInspectListScans.__init__')
 def test_webinspect_list_one_server(list_scan_mock, runner):
     list_scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'list', '--server', 'server1'])
 
     list_scan_mock.assert_called_once_with(None, ('server1',), None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectListScans.__init__')
 def test_webinspect_list_two_servers(list_scan_mock, runner):
     list_scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'list', '--server', 'server1', '--server', 'server2'])
 
     list_scan_mock.assert_called_once_with(None, ('server1', 'server2'), None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectListScans.__init__')
 def test_webinspect_list_two_servers_with_scan_name(list_scan_mock, runner):
     list_scan_mock.return_value = None
 
-    # Run the CLI proxy --list
-    runner.invoke(webbreaker, ['webinspect', 'list', '--server', 'server1', '--server', 'server2', '--scan_name', 'important_site'])
+    runner.invoke(webbreaker, ['webinspect', 'list', '--server', 'server1', '--server', 'server2', '--scan_name',
+                               'important_site'])
 
     list_scan_mock.assert_called_once_with('important_site', ('server1', 'server2'), None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectListScans.__init__')
 def test_webinspect_list_scans(list_scan_mock, runner):
     list_scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'list'])
 
     list_scan_mock.assert_called_once_with(None, (), None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectListScans.__init__')
 def test_webinspect_list_username_password(list_scan_mock, runner):
     list_scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'list', '--username', 'user', '--password', 'pass'])
 
     list_scan_mock.assert_called_once_with(None, (), 'user', 'pass')
+
 
 @mock.patch('webbreaker.__main__.WebInspectDownload.__init__')
 def test_webinspect_download(download_mock, runner):
     download_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'download', '--server', 'server', '--scan_name', 'important_site'])
 
     download_mock.assert_called_once_with('server', 'important_site', None, 'fpr', None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectDownload.__init__')
 def test_webinspect_download_username_password(download_mock, runner):
     download_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'download', '--server', 'server', '--scan_name', 'important_site',
                                '--username', 'user', '--password', 'pass'])
 
     download_mock.assert_called_once_with('server', 'important_site', None, 'fpr', 'user', 'pass')
 
+
 @mock.patch('webbreaker.__main__.WebInspectDownload.__init__')
 def test_webinspect_download_extension(download_mock, runner):
     download_mock.return_value = None
 
-    # Run the CLI proxy --list
-    runner.invoke(webbreaker, ['webinspect', 'download', '--server', 'server', '--scan_name', 'important_site', '-x', 'xml'])
+    runner.invoke(webbreaker, ['webinspect', 'download', '--server', 'server', '--scan_name', 'important_site', '-x',
+                               'xml'])
 
     download_mock.assert_called_once_with('server', 'important_site', None, 'xml', None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectDownload.__init__')
 def test_webinspect_download_scan_id(download_mock, runner):
     download_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'download', '--server', 'server', '--scan_name', 'important_site',
                                '--scan_id', 'my_important_scan_id'])
 
     download_mock.assert_called_once_with('server', 'important_site', 'my_important_scan_id', 'fpr', None, None)
 
+
 @mock.patch('webbreaker.__main__.WebInspectScan.__init__')
 def test_webinspect_scan(scan_mock, runner):
     scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'scan'])
 
     scan_mock.assert_called_once_with({'username': None, 'start_urls': (), 'scan_scope': None, 'upload_policy': None,
@@ -163,58 +166,62 @@ def test_webinspect_scan(scan_mock, runner):
                                        'scan_name': None, 'login_macro': None, 'scan_policy': None, 'allowed_hosts': (),
                                        'fortify_user': None, 'scan_mode': None})
 
+
 @mock.patch('webbreaker.__main__.WebInspectScan.__init__')
 def test_webinspect_scan_custom_settings(scan_mock, runner):
     scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'important_site_auth'])
 
     scan_mock.assert_called_once_with({'username': None, 'start_urls': (), 'scan_scope': None, 'upload_policy': None,
                                        'upload_webmacros': None, 'scan_start': None, 'workflow_macros': (),
-                                       'upload_settings': None, 'password': None, 'size': None, 'settings': 'important_site_auth',
-                                       'scan_name': None, 'login_macro': None, 'scan_policy': None, 'allowed_hosts': (),
-                                       'fortify_user': None, 'scan_mode': None})
+                                       'upload_settings': None, 'password': None, 'size': None,
+                                       'settings': 'important_site_auth', 'scan_name': None, 'login_macro': None,
+                                       'scan_policy': None, 'allowed_hosts': (), 'fortify_user': None,
+                                       'scan_mode': None})
+
 
 @mock.patch('webbreaker.__main__.WebInspectScan.__init__')
 def test_webinspect_scan_custom_settings_with_username_password(scan_mock, runner):
     scan_mock.return_value = None
 
-    # Run the CLI proxy --list
-    runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'important_site_auth', '--username', 'user', '--password', 'pass'])
+    runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'important_site_auth', '--username', 'user',
+                               '--password', 'pass'])
 
     scan_mock.assert_called_once_with({'username': 'user', 'start_urls': (), 'scan_scope': None, 'upload_policy': None,
                                        'upload_webmacros': None, 'scan_start': None, 'workflow_macros': (),
-                                       'upload_settings': None, 'password': 'pass', 'size': None, 'settings': 'important_site_auth',
-                                       'scan_name': None, 'login_macro': None, 'scan_policy': None, 'allowed_hosts': (),
-                                       'fortify_user': None, 'scan_mode': None})
+                                       'upload_settings': None, 'password': 'pass', 'size': None,
+                                       'settings': 'important_site_auth', 'scan_name': None, 'login_macro': None,
+                                       'scan_policy': None, 'allowed_hosts': (), 'fortify_user': None, 'scan_mode': None})
+
 
 @mock.patch('webbreaker.__main__.WebInspectScan.__init__')
 def test_webinspect_scan_custom_settings_with_two_allowed_hosts(scan_mock, runner):
     scan_mock.return_value = None
 
-    # Run the CLI proxy --list
-    runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'important_site_auth', '--allowed_hosts', 'example.com',
-                               '--allowed_hosts', 'example1.com'])
+    runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', 'important_site_auth', '--allowed_hosts',
+                               'example.com', '--allowed_hosts', 'example1.com'])
 
     scan_mock.assert_called_once_with({'username': None, 'start_urls': (), 'scan_scope': None, 'upload_policy': None,
                                        'upload_webmacros': None, 'scan_start': None, 'workflow_macros': (),
-                                       'upload_settings': None, 'password': None, 'size': None, 'settings': 'important_site_auth',
-                                       'scan_name': None, 'login_macro': None, 'scan_policy': None,
-                                       'allowed_hosts': ('example.com', 'example1.com',), 'fortify_user': None, 'scan_mode': None})
+                                       'upload_settings': None, 'password': None, 'size': None,
+                                       'settings': 'important_site_auth', 'scan_name': None, 'login_macro': None,
+                                       'scan_policy': None, 'allowed_hosts': ('example.com', 'example1.com',),
+                                       'fortify_user': None, 'scan_mode': None})
+
 
 @mock.patch('webbreaker.__main__.WebInspectScan.__init__')
 def test_webinspect_scan_local_custom_settings(scan_mock, runner):
     scan_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'scan', '--settings', '/users/me/important_site_auth'])
 
     scan_mock.assert_called_once_with({'username': None, 'start_urls': (), 'scan_scope': None, 'upload_policy': None,
                                        'upload_webmacros': None, 'scan_start': None, 'workflow_macros': (),
-                                       'upload_settings': None, 'password': None, 'size': None, 'settings': '/users/me/important_site_auth',
-                                       'scan_name': None, 'login_macro': None, 'scan_policy': None,
-                                       'allowed_hosts': (), 'fortify_user': None, 'scan_mode': None})
+                                       'upload_settings': None, 'password': None, 'size': None,
+                                       'settings': '/users/me/important_site_auth', 'scan_name': None,
+                                       'login_macro': None, 'scan_policy': None, 'allowed_hosts': (),
+                                       'fortify_user': None, 'scan_mode': None})
 
 
 @mock.patch('webbreaker.webinspect.scan.create_scan_event_handler')
@@ -270,7 +277,6 @@ def test_webinspect_list_servers(test_mock, runner, caplog):
 def test_webinspect_proxy_list_available_proxies(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--list', '--server', 'https://test-server'])
 
     proxy_mock.assert_called_once_with(False, True, None, None, False, 'https://test-server', False, False, None, False,
@@ -281,57 +287,56 @@ def test_webinspect_proxy_list_available_proxies(proxy_mock, runner):
 def test_webinspect_proxy_start(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--start', '--proxy_name', 'test'])
 
     proxy_mock.assert_called_once_with(False, False, None, 'test', False, None, True, False, None, False,
                                        None, None)
 
+
 @mock.patch('webbreaker.__main__.WebInspectProxy.__init__')
 def test_webinspect_proxy_start_with_port_name(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--start','--port', '9001', '--proxy_name', 'test'])
 
     proxy_mock.assert_called_once_with(False, False, '9001', 'test', False, None, True, False, None, False,
                                        None, None)
 
+
 @mock.patch('webbreaker.__main__.WebInspectProxy.__init__')
 def test_webinspect_proxy_download_webmacro(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--download', '--webmacro', '--proxy_name', 'test'])
 
     proxy_mock.assert_called_once_with(True, False, None, 'test', False, None, False, False, None, True,
                                        None, None)
 
+
 @mock.patch('webbreaker.__main__.WebInspectProxy.__init__')
 def test_webinspect_proxy_download_setting(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--download', '--setting', '--proxy_name', 'test'])
 
     proxy_mock.assert_called_once_with(True, False, None, 'test', True, None, False, False, None, False,
                                        None, None)
 
+
 @mock.patch('webbreaker.__main__.WebInspectProxy.__init__')
 def test_webinspect_proxy_upload(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--upload', 'test-proxy.webmacro', '--proxy_name', 'test'])
 
-    proxy_mock.assert_called_once_with(False, False, None, 'test', False, None, False, False, 'test-proxy.webmacro', False,
-                                       None, None)
+    proxy_mock.assert_called_once_with(False, False, None, 'test', False, None, False, False, 'test-proxy.webmacro',
+                                       False, None, None)
+
 
 @mock.patch('webbreaker.__main__.WebInspectProxy.__init__')
 def test_webinspect_proxy_stop(proxy_mock, runner):
     proxy_mock.return_value = None
 
-    # Run the CLI proxy --list
     runner.invoke(webbreaker, ['webinspect', 'proxy', '--stop', '--proxy_name', 'test'])
 
     proxy_mock.assert_called_once_with(False, False, None, 'test', False, None, False, True, None, False,
