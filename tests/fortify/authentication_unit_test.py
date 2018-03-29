fdd2393e7fa885a885a557e8b9de6c9cd68578aa
KylerWitting <kylerwitting@gmail.com>
2018-03-29 14:11:09 -0500

    Correct method name in fortify test

--- tests/fortify/authentication_unit_test.py
+++ tests/fortify/authentication_unit_test.py
@@ -86,7 +86,7 @@ def test_authenticate_has_credentials_success(secret_mock):
     secret_mock.return_value.get.side_effect = ["config_username", "config_password"]
 
     fortify_auth = FortifyAuth()
-    result = fortify_auth.has_auth_creds()
+    result = fortify_auth._has_auth_creds()
 
     assert result is True
 
@@ -97,6 +97,6 @@ def test_authenticate_has_credentials_fail(secret_mock):
     secret_mock.return_value.get.side_effect = [None, None]
 
     fortify_auth = FortifyAuth()
-    result = fortify_auth.has_auth_creds()
+    result = fortify_auth._has_auth_creds()
 
     assert result is False
