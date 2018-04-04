886c6f945404cf23f5fa1fbfe809dffa9e6845db
KylerWitting <kylerwitting@gmail.com>
2018-04-04 14:21:14 -0500

    Reorder helper methods

--- webbreaker/fortify/common/helper.py
+++ webbreaker/fortify/common/helper.py
@@ -78,7 +78,7 @@ class FortifyHelper(object):
             if self._get_version_id(application_name, version_name):
                 Logger.app.error(
                     "Found existing Application Version '{} : {}'. Unable to upload.".format(application_name,
-                                                                                          version_name))
+                                                                                             version_name))
                 sys.exit(ExitStatus.failure)
             # Application exists but not Version
             else:
@@ -100,77 +100,13 @@ class FortifyHelper(object):
             "Your scan file {0}.{1}, has been successfully uploaded to {2}!".format(file_name, self.extension,
                                                                                     self.fortify_url))
 
-    def _get_application_id(self, application_name):
+    def _commit_version(self, version_id):
         """
-        Returns the ID of the specified Application. Project is a deprecated name for Application.
-        :return: ID of the Application
+        Commit a Version to Fortify. Necessary step when creating new Versions OR uploading new attributes.
+        :param version_id: Version ID to commit.
         """
-
-        response = self.api.get_projects()
+        response = self.api.commit_project_version(project_version_id=version_id)
         APIHelper().check_for_response_errors(response)
-
-        for application in response.data['data']:
-            if application['name'] == application_name:
-                return application['id']
-        return None
-
-    def _get_version_id(self, application_name, version_name):
-        """
-        Returns the ID of the specified Application. Project is a deprecated name for Application
-        :return: ID of the Version
-        """
-        response = self.api.get_all_project_versions()
-        APIHelper().check_for_response_errors(response)
-
-        for version in response.data['data']:
-            if version['project']['name'] == application_name:
-                if version['name'] == version_name:
-                    return version['id']
-        return None
-
-    def _get_applications_and_versions(self):
-        """
-        Gets every application & Version for listing. It returns the response data after checking for errors.
-        :return: Response data that will be used to list Applications & Versions
-        """
-        response = self.api.get_all_project_versions()
-        APIHelper().check_for_response_errors(response)
-        return response.data['data']
-
-    def _download_version(self, version_id):
-        """
-        Downloads the Version specified, checks for errors, then returns the data & file name (Version name)
-        :param version_id: Version ID to download
-        :return: Response data that will be written to the file & the file_name of where to download it.
-        """
-        response, file_name = self.api.download_artifact_scan(version_id)
-        APIHelper().check_for_response_errors(response)
-        return response.data, file_name
-
-    def _create_version(self, application_name, application_id,
-                        version_name, application_template, version_description):
-        """
-        Creates a new Version under the specified Application ID.
-        :param application_name: Name of the Application to put the Version under.
-        :param application_id: ID of the Application. If None, creates a new Application with the application_name.
-        :param version_name: Name of the Version to create.
-        :param application_template: Brought in from the config.ini during the FortifyUpload Class __init__
-        :param version_description: Version description for Application Version
-        :return: Version ID of the newly created Version.
-        """
-
-        response = self.api.create_project_version(project_name=application_name,
-                                                   project_id=application_id,
-                                                   project_template=application_template,
-                                                   version_name=version_name,
-                                                   description=version_description)
-        APIHelper().check_for_response_errors(response)
-        version_id = response.data['data']['id']
-
-        self._upload_version_attributes(version_id)
-        self._commit_version(version_id)
-
-        return version_id
 
     def _create_application_and_version(self, application_name, version_name,
                                         application_template, version_description):
@@ -203,47 +139,63 @@ class FortifyHelper(object):
         APIHelper().check_for_response_errors(response)
         return version_id
 
-    def _upload_version_attributes(self, version_id):
+    def _create_version(self, application_name, application_id,
+                        version_name, application_template, version_description):
         """
-        Uploads attributes to the specified version_id. Will grab all attributes to upload from config.ini
-        :param version_id: Version to upload attributes to.
+        Creates a new Version under the specified Application ID.
+        :param application_name: Name of the Application to put the Version under.
+        :param application_id: ID of the Application. If None, creates a new Application with the application_name.
+        :param version_name: Name of the Version to create.
+        :param application_template: Brought in from the config.ini during the FortifyUpload Class __init__
+        :param version_description: Version description for Application Version
+        :return: Version ID of the newly created Version.
         """
-        search_expression = self.config.search_expression
-        attribute_definition_id = self.config.attribute_definition_id
 
-        version_attribute_value = self.config.version_attribute_value
-        version_attribute_values = self.config.version_attribute_values
+        response = self.api.create_project_version(project_name=application_name,
+                                                   project_id=application_id,
+                                                   project_template=application_template,
+                                                   version_name=version_name,
+                                                   description=version_description)
+        APIHelper().check_for_response_errors(response)
+        version_id = response.data['data']['id']
 
-        if search_expression is not '':
-            attribute_definition_id = self._get_attribute_definition_id(search_expression=search_expression)
-        if version_attribute_values is '':
-            version_attribute_values = []
+        self._upload_version_attributes(version_id)
+        self._commit_version(version_id)
 
-        response = self.api.add_project_version_attribute(project_version_id=version_id,
-                                                          attribute_definition_id=attribute_definition_id,
-                                                          value=version_attribute_value,
-                                                          values=version_attribute_values)
+        return version_id
 
+    def _download_version(self, version_id):
+        """
+        Downloads the Version specified, checks for errors, then returns the data & file name (Version name)
+        :param version_id: Version ID to download
+        :return: Response data that will be written to the file & the file_name of where to download it.
+        """
+        response, file_name = self.api.download_artifact_scan(version_id)
+        APIHelper().check_for_response_errors(response)
+        return response.data, file_name
+
+    def _get_application_id(self, application_name):
+        """
+        Returns the ID of the specified Application. Project is a deprecated name for Application.
+        :return: ID of the Application
+        """
+
+        response = self.api.get_projects()
         APIHelper().check_for_response_errors(response)
 
-    def _commit_version(self, version_id):
-        """
-        Commit a Version to Fortify. Necessary step when creating new Versions OR uploading new attributes.
-        :param version_id: Version ID to commit.
-        """
-        response = self.api.commit_project_version(project_version_id=version_id)
-        APIHelper().check_for_response_errors(response)
+        for application in response.data['data']:
+            if application['name'] == application_name:
+                return application['id']
+        return None
 
-    def _upload_application_version_file(self, version_id, file_name):
-        response = self.api.upload_artifact_scan(file_path=('{0}.{1}'.format(file_name, self.extension)),
-                                                 project_version_id=version_id)
+    def _get_applications_and_versions(self):
+        """
+        Gets every application & Version for listing. It returns the response data after checking for errors.
+        :return: Response data that will be used to list Applications & Versions
+        """
+        response = self.api.get_all_project_versions()
         APIHelper().check_for_response_errors(response)
-
-    def _project_version_description(self):
-        if self.runenv == "jenkins":
-            return "WebInspect scan from WebBreaker " + os.getenv('JOB_URL', "jenkins server")
-        else:
-            return "WebBreaker scan from WebBreaker host " + socket.getfqdn()
+        return response.data['data']
 
     def _get_attribute_definition_id(self, search_expression):
         response = self.api.get_attribute_definition(search_expression=search_expression)
@@ -251,6 +203,26 @@ class FortifyHelper(object):
             if response.data['data'] is not []:
                 return response.data['data'][0]['id']
         return None
+
+    def _get_version_id(self, application_name, version_name):
+        """
+        Returns the ID of the specified Application. Project is a deprecated name for Application
+        :return: ID of the Version
+        """
+        response = self.api.get_all_project_versions()
+        APIHelper().check_for_response_errors(response)
+
+        for version in response.data['data']:
+            if version['project']['name'] == application_name:
+                if version['name'] == version_name:
+                    return version['id']
+        return None
+
+    def _project_version_description(self):
+        if self.runenv == "jenkins":
+            return "WebInspect scan from WebBreaker " + os.getenv('JOB_URL', "jenkins server")
+        else:
+            return "WebBreaker scan from WebBreaker host " + socket.getfqdn()
 
     def _setup_fortify_ssc_api(self):
         """
@@ -274,6 +246,34 @@ class FortifyHelper(object):
                 Logger.app.critical("Exception while getting Fortify token: {0}".format(e.message))
 
         return None
+
+    def _upload_application_version_file(self, version_id, file_name):
+        response = self.api.upload_artifact_scan(file_path=('{0}.{1}'.format(file_name, self.extension)),
+                                                 project_version_id=version_id)
+        APIHelper().check_for_response_errors(response)
+
+    def _upload_version_attributes(self, version_id):
+        """
+        Uploads attributes to the specified version_id. Will grab all attributes to upload from config.ini
+        :param version_id: Version to upload attributes to.
+        """
+        search_expression = self.config.search_expression
+        attribute_definition_id = self.config.attribute_definition_id
+
+        version_attribute_value = self.config.version_attribute_value
+        version_attribute_values = self.config.version_attribute_values
+
+        if search_expression is not '':
+            attribute_definition_id = self._get_attribute_definition_id(search_expression=search_expression)
+        if version_attribute_values is '':
+            version_attribute_values = []
+
+        response = self.api.add_project_version_attribute(project_version_id=version_id,
+                                                          attribute_definition_id=attribute_definition_id,
+                                                          value=version_attribute_value,
+                                                          values=version_attribute_values)
+
+        APIHelper().check_for_response_errors(response)
 
     @staticmethod
     def _trim_ext(file):
